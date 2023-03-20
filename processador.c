c2fd0f839d0c5980972a0117b2bc580fe93a9d4f
izuca <adriel200010@hotmail.com>
2023-03-20 18:26:28 -0300

    first commit

---
+++ processador.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Função main
+int main (){
+    
+    //Declarando variáveis
+    unsigned int mbr;
+    unsigned short int mar, ibr, imm, pc, a, b, t;
+    unsigned char ir,e,l,g;
+
+}
