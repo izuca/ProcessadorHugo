6a19eb064a612e8d6bdaf2e52e6a09c91d608a57
adriel.santos <adriel200010@hotmail.com>
2023-03-28 23:57:13 -0300

    minor changes

--- processador.c
+++ processador.c
@@ -11,8 +11,12 @@ int main (){
     unsigned char ir,e,l,g,memoria[154];
     
     //testando ...
+
+    //1001 1000 0000 1100 1010 0000 0000 1110  | lda C ldb E
     memoria[1] = "10011000";
-    memoria[2] = "10011000";
+    memoria[2] = "00001100";
+    memoria[3] = "10100000";
+    memoria[4] = "00001110";
     hello();
 
 }
